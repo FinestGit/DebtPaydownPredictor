367976be94aced87d2fa8d55065e04b3bdb64ec0
FinestGit <chaserogness@gmail.com>
2023-02-03 13:52:28 -0600

    Completed initial title with styling of Debt Input Component

--- src/components/DebtInputComponent.tsx
+++ src/components/DebtInputComponent.tsx
@@ -2,8 +2,8 @@ import "../styles/DebtInputComponent.scss";
 
 function DebtInputComponent() {
   return (
-    <div className="debt-input-container">
-      <h4 className="debt-input-title">Input Debt</h4>
+    <div className="debt-input-component-container">
+      <h4 className="debt-input-component-title">Input Debt</h4>
       {/* Debt Name Input */}
       {/* Total Amount Due Input */}
       {/* Monthly Payment Input */}
