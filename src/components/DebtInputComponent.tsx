2576a446e20542a1995f7144b2aabdf7db82ea06
Chase Rogness <5387800+FinestGit@users.noreply.github.com>
2023-02-03 14:37:03 -0600

    Debt Input Component Initial Setup (#1)
    * Completed initial title with styling of Debt Input Component
    
    * Created initial input component for debt name
    
    * Debt Input Component initial version complete

--- src/components/DebtInputComponent.tsx
+++ src/components/DebtInputComponent.tsx
@@ -2,14 +2,27 @@ import "../styles/DebtInputComponent.scss";
 
 function DebtInputComponent() {
   return (
-    <div className="debt-input-container">
-      <h4 className="debt-input-title">Input Debt</h4>
-      {/* Debt Name Input */}
-      {/* Total Amount Due Input */}
-      {/* Monthly Payment Input */}
-      {/* APR Input */}
-      {/* Cancel Button */}
-      {/* Save Button */}
+    <div className="debt-input-component-container">
+      <h4 className="debt-input-component-title">Input Debt</h4>
+      <div className="debt-name-input">
+        <label>Debt Name</label>
+        <input></input>
+      </div>
+      <div className="debt-total-due-input">
+        <label>Total Amount Due</label>
+        <input></input>
+      </div>
+      <div className="debt-monthly-payment-input">
+        <label>Monthly Payment</label>
+        <input></input>
+      </div>
+      <div className="debt-apr-input">
+        <label>APR</label>
+        <input></input>
+      </div>
+      <div className="debt-input-spacer"></div>
+      <button className="debt-input-cancel">Cancel</button>
+      <button className="debt-input-save">Save Debt</button>
     </div>
   );
 }
