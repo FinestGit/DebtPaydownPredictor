77ed71f353423bb82fac311ce9a6ade7eb7396a1
FinestGit <chaserogness@gmail.com>
2023-02-03 13:29:03 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import DebtInputComponent from "./components/DebtInputComponent";
+
+function App() {
+  return (
+    <div>
+      <DebtInputComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DebtInputComponent.tsx
@@ -0,0 +1,17 @@
+import "../styles/DebtInputComponent.scss";
+
+function DebtInputComponent() {
+  return (
+    <div className="debt-input-container">
+      <h4 className="debt-input-title">Input Debt</h4>
+      {/* Debt Name Input */}
+      {/* Total Amount Due Input */}
+      {/* Monthly Payment Input */}
+      {/* APR Input */}
+      {/* Cancel Button */}
+      {/* Save Button */}
+    </div>
+  );
+}
+
+export default DebtInputComponent;
