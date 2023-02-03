7cf19c9a8d5b2fbf5615a7ffb7b7fd1ae21859f9
FinestGit <chaserogness@gmail.com>
2023-02-03 14:05:30 -0600

    Created initial input component for debt name

--- src/components/DebtInputComponent.tsx
+++ src/components/DebtInputComponent.tsx
@@ -4,12 +4,15 @@ function DebtInputComponent() {
   return (
     <div className="debt-input-component-container">
       <h4 className="debt-input-component-title">Input Debt</h4>
-      {/* Debt Name Input */}
-      {/* Total Amount Due Input */}
-      {/* Monthly Payment Input */}
-      {/* APR Input */}
-      {/* Cancel Button */}
-      {/* Save Button */}
+      <div className="debt-name-input">
+        <label>Debt Name</label>
+        <input></input>
+      </div>
+      {/* TODO: Total Amount Due Input */}
+      {/* TODO: Monthly Payment Input */}
+      {/* TODO: APR Input */}
+      {/* TODO: Cancel Button */}
+      {/* TODO: Save Button */}
     </div>
   );
 }
